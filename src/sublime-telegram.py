2ae3319fe85e27f76eb5d38255eb01c906a47cc8
Mehmet Ali Gozaydin <kubudik@gmail.com>
2016-01-23 14:54:30 +0200

    telegram connection

--- src/sublime-telegram.py
+++ src/sublime-telegram.py
@@ -1,11 +1,21 @@
 import sublime, sublime_plugin
+from pytg import Telegram
+import sys
+from pytg.sender import Sender
+from pytg.receiver import Receiver
 
-class ExampleCommand(sublime_plugin.TextCommand):
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class TelegramCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    view = self.view
-    sel = view.sel()[0]
-    print(sel)
-    line_pos = view.line(sel)
-    print(line_pos)
-    lala = self.view.substr(line_pos)
-    print(lala)
+    print("alala")
+    connect_to_telegram()
+
+  def connect_to_telegram(self):
+    receiver = Receiver(host="localhost", port=9091)
+    sender = Sender(host="localhost", port=9091)
+
+    sender.send_msg(u'USERNAME', u'Hello World!')
+    list = sender.contacts_list() 
+    print(list)
